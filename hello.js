3463f9262e00c3b9fa12b2267c595fb50f7a5e0d
nokgp <hathaichanok.hg@gmail.com>
2022-03-01 09:16:34 +0000

    first commit

---
+++ hello.js
@@ -0,0 +1,8 @@
+let words = ['French: Je t\'aime','Spanish: Te quiero','Italian: Ti amo',
+                'English: I love you','German: Ich liebe dich','Korean: Salanghae','Italian: Ti amo',
+                'Thai: Phom rak khun','Dutch: Ik hou van je','Chinese: Wo ai ni','Arabic: Ahabak',
+                'Portuguese: eu te amo','Malay/Indonesian: Saya sayang awak','Vietnamese: Anh yêu em',
+                'Turkish: Seni seviyorum','Tagalog: Iniibig kita'];
+
+let random = words[Math.floor(Math.random() * words.length)];
+console.log(random);
